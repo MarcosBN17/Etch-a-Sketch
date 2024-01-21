84762d33a7c8cea2a86689d9b1f584a5748db2c7
MarcosBN17 <marcosbneves12@gmail.com>
2024-01-21 16:37:21 +0000

    v0.3 - Added a colorPicker and a Reset button

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
 //Container div
 const container = document.querySelector('#container');
+const colorPicker = document.getElementById('colorPicker');
+const clearButton = document.getElementById('clearButton');
+
+//Color Picker function
+colorPicker.addEventListener('input', function() {
+    const selectedColor = colorPicker.value;
+    document.documentElement.style.setProperty('--selected-color', colorPicker.value);
+});
+
+//Reset button function
+clearButton.addEventListener('click', function() {
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => {
+        square.style.backgroundColor = '';
+    });
+});
+
+
+
 
 //16x16 grid of square divs
 for (let i= 0; i < 16; i++) {
@@ -10,7 +29,10 @@ for (let i= 0; i < 16; i++) {
 
         //Add a class to the div for styling
         square.classList.add('grid-square');
-        //square.textContent = "Hello";
+        
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = document.documentElement.style.getPropertyValue('--selected-color');
+        });
 
         //Append the div to the container
         container.appendChild(square);
