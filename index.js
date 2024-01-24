44134f7365a9cfe89d8cfe93c961fab206350698
MarcosBN17 <marcosbneves12@gmail.com>
2024-01-24 21:30:24 +0000

    V1.0 - Fixed error styles, added labels

--- index.js
+++ index.js
@@ -29,11 +29,27 @@ document.addEventListener('DOMContentLoaded', function() {
     clearButton.setAttribute('id', 'clearButton');
     clearButton.textContent = 'Clear';
 
+    //Create slide
+    const gridSlide = document.createElement('input');
+    gridSlide.setAttribute('type', 'range');
+    gridSlide.setAttribute('min', '8');
+    gridSlide.setAttribute('max', '64');
+    gridSlide.setAttribute('value', '18');
+    gridSlide.setAttribute('class', 'input-slider');
+    gridSlide.setAttribute('id', 'slider');
+
+    //Create slide label
+    const sliderLabel = document.createElement('label');
+    sliderLabel.setAttribute('for', 'slider');
+    sliderLabel.textContent = 'Choose grid size:';
+
     //Append elements to sidebox div
     sidebox.appendChild(heading);
     sidebox.appendChild(label);
     sidebox.appendChild(colorPicker);
     sidebox.appendChild(clearButton);
+    sidebox.appendChild(sliderLabel);
+    sidebox.appendChild(gridSlide);
 
     //Append sidebox to wrapper div
     wrapper.appendChild(sidebox);
@@ -51,6 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //Container div
     const colorpicker = document.getElementById('colorPicker');
     const clearbutton = document.getElementById('clearButton');
+    const slider = document.getElementById('slider');
 
     // Color Picker function
     colorpicker.addEventListener('input', function() {
@@ -62,28 +79,33 @@ document.addEventListener('DOMContentLoaded', function() {
     clearbutton.addEventListener('click', function() {
         const squares = document.querySelectorAll('.grid-square');
         squares.forEach(square => {
+            square.style.backgroundColor = '';
             square.style.backgroundColor = 'transparent';
             square.style.transition = 'background-color 0.5s ease';
         });
     });
 
-    // 16x16 grid of square divs
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            // Create new div element
+    slider.addEventListener("change", () => {
+        let squares = slider.value;
+
+        container.innerHTML = "";
+
+        container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+
+        for (let i = 0; i < (squares * squares); i++) {
             let square = document.createElement('div');
+            square.classList.add("grid-square");
 
-            // Add a class to the div for styling
-            square.classList.add('grid-square');
-
+            //Add event listener for hover effect
             square.addEventListener('mouseover', function() {
                 square.style.backgroundColor = document.documentElement.style.getPropertyValue('--selected-color');
-            });
 
-            // Append the div to the container
+            });
             container.appendChild(square);
         }
-    }
+    });   
+    
 
     // Retrieve the placeholder and append the footer container
     const footerContainer = document.createElement('div');
@@ -96,7 +118,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(footerContainer);
 
 });
-
 
 
 
